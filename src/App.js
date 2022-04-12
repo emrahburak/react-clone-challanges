c822e3e2a02c0f0c0a4cd62beb48c6abcfc13efc
emrahburak <gemrahburak@gmail.com>
2022-04-12 17:12:26 +0300

    homepagelayout --done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
+import HomepageLayout from "../src/layouts/HomepageLayout";
+
+import Homepage from "../src/pages/Homepage";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
